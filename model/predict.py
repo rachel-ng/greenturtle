4102788308a3d4b58f702768cf7fa4ec7f975bbc
rachel <rng@stuy.edu>
2021-01-13 20:48:12 -0500

    .

--- model/predict.py
+++ model/predict.py
@@ -1,12 +1,9 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 import PIL
 import tensorflow as tf
 
 from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
 
 model = tf.keras.models.load_model('model/my_model.h5')
 
