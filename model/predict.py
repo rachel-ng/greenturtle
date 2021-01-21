49193e86bb8d280320e62036b602bb7ed6f7bd36
rachel <rng@stuy.edu>
2021-01-21 03:21:26 -0500

    fixed dictionary, random string as img name

--- model/predict.py
+++ model/predict.py
@@ -1,8 +1,9 @@
-import numpy as np
 import os
+import string
+import random
 import PIL
+import numpy as np
 import tensorflow as tf
-
 from tensorflow import keras
 
 model = tf.keras.models.load_model('model/my_model.h5')
@@ -11,7 +12,8 @@ def make_prediction(data):
     img_height = 180
     img_width = 180
     img_url = data 
-    img_path = tf.keras.utils.get_file('someimage', origin=img_url)
+    img_name = ''.join(random.choice(string.ascii_letters) for i in range(10))
+    img_path = tf.keras.utils.get_file(img_name, origin=img_url)
     img = keras.preprocessing.image.load_img(
         img_path, target_size=(img_height, img_width)
     )
@@ -20,4 +22,4 @@ def make_prediction(data):
     prediction = model.predict(img_array)
     score = tf.nn.softmax(prediction[0])
     class_names=['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']
-    return {'prediction':prediction,'message':"most likely a {} with {:.2f} % confidence.".format(class_names[np.argmax(score)], 100 * np.max(score)),'img':data}
+    return {'message':"most likely a {} with {:.2f} % confidence.".format(class_names[np.argmax(score)], 100 * np.max(score)),'img':data}
