e9cdadb08f58bb2f9b56af4825548ac15486e688
rachel <rng@stuy.edu>
2021-01-21 04:10:09 -0500

    json response, headers, ssl for https connection

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
 import os, sys
 import json
-from flask import Flask,request,render_template
+from flask import Flask, request, jsonify, send_from_directory
 from model import predict as p
+
 # create the flask object
 app = Flask(__name__)
 port = int(os.environ.get("PORT", 5000))
 
 @app.route('/')
 def index():
-    return "Index Page"
+    return "welcome to greenturtle"
+
+@app.route('/favicon.ico') 
+def favicon(): 
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
+
 @app.route('/predict',methods=['GET','POST'])
 def predict():
     data = request.args.get('data') or request.form.get('data')
     if data == None:
-        return 'Got None'
+        return 'no data'
     else:
         prediction = p.make_prediction(data) 
-    return json.dumps(str(prediction))
+
+    response = jsonify(prediction)
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("content-type", "application/json")
+    return response 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, ssl_context='adhoc', debug=True)
 
