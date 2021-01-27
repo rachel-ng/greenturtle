1c9a5f9eae10619411a1797f774a153ae2862826
rachel <rng@stuy.edu>
2021-01-26 23:58:58 -0500

    api usage

--- api.py
+++ api.py
@@ -3,9 +3,13 @@ import json
 
 def main():
     PARAMS = {'data':'https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg'}
-    response = requests.post(url='https://localhost:5000/predict?', data=PARAMS, verify=False)
+    #endpoint = 'https://localhost:5000/predict'
+    endpoint = 'https://greenturtle.herokuapp.com/predict'
+    response = requests.post(url=endpoint, data=PARAMS, verify=False)
     print(response)
     print(response.text)
+    print(type(response.text))
+    print(type(response))
 
 if __name__ == "__main__":
     main()
