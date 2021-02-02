064c41472b3a508c9318c30eeaf90021121e5c59
rachel <rng@stuy.edu>
2021-02-01 19:17:47 -0500

    switch model

--- model/predict.py
+++ model/predict.py
@@ -6,7 +6,8 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
-model = tf.keras.models.load_model('model/my_model.h5')
+# model = tf.keras.models.load_model('model/my_model.h5')
+model = tf.keras.models.load_model('model/access-a-meme_model.h5')
 
 def make_prediction(data):
     img_height = 180
@@ -21,5 +22,7 @@ def make_prediction(data):
     img_array = tf.expand_dims(img_array, 0) 
     prediction = model.predict(img_array)
     score = tf.nn.softmax(prediction[0])
-    class_names=['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']
+    ##class_names=['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']
+    class_names=['do-a-barrel-roll', 'doge', 'dolan', 'ermahgerd', 'flipping-tables', 'forever-alone', 'lenny-face', 'loss', 'me-gusta', 'my-little-pony-friendship-is-magic', 'navy-seal-copypasta', 'slender-man', 'trollface', 'ugandan-knuckles', 'yao-ming-face-bitch-please']
     return {'prediction':"most likely a {} with {:.2f}% confidence.".format(class_names[np.argmax(score)], 100 * np.max(score))}
+
